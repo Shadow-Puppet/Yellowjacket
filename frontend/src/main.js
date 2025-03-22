26c02b27ffcf8c80408163167356550e49500d04
Logan Jones <logan@ljones.me>
2025-03-22 02:42:42 -0500

    icons and slight refactor

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,13 +2,13 @@ import './pico.css';
 import { DirectoryPicker } from '../wailsjs/go/main/App';
 
 
-let libraryDirectoryElement = document.getElementById("library-directory");
+let libraryDirectoryLabel = document.getElementById("library-directory-label");
 
 window.dirPicker = function () {
   var dir;
   try {
     DirectoryPicker()
-    .then((result) => {libraryDirectoryElement.innerText = result;})
+    .then((result) => {libraryDirectoryLabel.innerText = result;})
     .catch((err) => {
       console.log("There is an error with directory picker")
       console.error(err);});
