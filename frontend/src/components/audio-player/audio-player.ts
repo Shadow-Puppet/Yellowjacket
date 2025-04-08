8998c1495482ec60dfebc6cc6fd8a4ae4eedd6b8
Logan Jones <ljones@miletwo.us>
2025-04-08 12:11:16 -0500

    working on redoing ts and vite conf

--- frontend/src/components/audio-player/audio-player.ts
+++ frontend/src/components/audio-player/audio-player.ts
@@ -1,6 +1,5 @@
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import { Play } from '@go/player/Player';
 import './controls/player-controls';
 import './seekbar/seek-bar';
 
@@ -14,7 +13,7 @@ const audioPlayer = () => html`
 @customElement('audio-player')
 export class AudioPlayer extends LitElement {
 
-  render() {
+  override render() {
     return audioPlayer();
   }
 
