792ee630db6eb0a67ca816f93c216ebe5440bda2
Logan Jones <logan@ljones.me>
2025-04-16 12:38:08 -0500

    fixed missing columns

--- backend/database/sql/schemas/artist_credit_artist.sql
+++ backend/database/sql/schemas/artist_credit_artist.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS artist_credit_artist (
   id   int PRIMARY KEY,
+  artist_id int NOT NULL,
+  credit_id int NOT NULL,
   FOREIGN KEY(artist_id) REFERENCES artists(id),
   FOREIGN KEY(credit_id) REFERENCES artist_credit(id)
 );
--- backend/database/sql/schemas/audio_files.sql
+++ backend/database/sql/schemas/audio_files.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS audio_files (
   id int PRIMARY KEY,
   file_path text NOT NULL UNIQUE,
   length_milliseconds int NOT NULL,
+  file_type_id int NOT NULL,
+  recording_id int NOT NULL,
   FOREIGN KEY(file_type_id) REFERENCES file_types(id),
   FOREIGN KEY(recording_id) REFERENCES recordings(id)
 );
